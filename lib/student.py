f581eec7b43bea4b6213d9ab32fb414aae0b970f
alchemist1111 <emoyoeugine@gmail.com>
2024-05-29 20:38:02 +0300

    adds solution

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,15 @@
 from user import User
 
 class Student(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, knowledge_string):
+        self.knowledge.append(knowledge_string)
+        return knowledge_string
     
-    def learn(self):
-        pass
+    def know(self):
+        return self.knowledge
+    
+    
